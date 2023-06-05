4e31c5a7b8e8b2224854cc031e51f1d41aaa987d
saikrupa224 <saikrupareddymiriyala@gmail.com>
2023-06-05 13:03:42 -0500

    Assignment

---
+++ src/Day0405Assignments/Question1.java
@@ -0,0 +1,30 @@
+package Day0405Assignments;
+// First Interface
+interface A {
+    void M1();
+}
+
+// Second Interface
+interface B {
+    void M2();
+}
+
+// Class implementing both interfaces
+class Test implements A, B {
+    @Override
+    public void M1() {
+        System.out.println("Hello");
+    }
+
+    @Override
+    public void M2() {
+        System.out.println("Welcome");
+    }
+}
+public class Question1 {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.M1();
+        test.M2();
+    }
+}
