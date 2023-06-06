6a2932f92af79ce55b53f193c9dde41948311b4f
saikrupa224 <saikrupareddymiriyala@gmail.com>
2023-06-06 12:21:47 -0500

    Assignment

---
+++ src/Day06Assignment/Age.java
@@ -0,0 +1,25 @@
+package Day06Assignment;
+
+public class Age {
+    public void age(int age) {
+        if(age>=13 && age<=19)
+        {
+            System.out.println("Teen");
+        }
+        else if(age<13)
+        {
+            System.out.println("Kid");
+        }
+        else if(age>19)
+        {
+            System.out.println("Adult");
+        }
+    }
+
+    public static void main(String[]args)
+    {
+        Age age=new Age();
+        age.age(10);
+
+    }
+}
---
+++ src/Day06Assignment/NumberReverse.java
@@ -0,0 +1,18 @@
+package Day06Assignment;
+
+public class NumberReverse {
+    int reverse =0;
+    public int NumberReverse(int number) {
+
+        while (number != 0) {
+            int reminder = number % 10;
+            reverse = reverse * 10 + reminder;
+            number = number / 10;
+        }
+        return reverse;
+    }
+    public static void main(String[] args) {
+        NumberReverse nr= new NumberReverse();
+        System.out.println("The reversed number is "+nr.NumberReverse(120469));
+    }
+}
