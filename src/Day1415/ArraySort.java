4b10b767e0a70c540ac178abecb2a738c4becda6
saikrupa224 <saikrupareddymiriyala@gmail.com>
2023-06-19 14:54:44 -0500

    Added

---
+++ src/Day1415/ArraySort.java
@@ -0,0 +1,16 @@
+package Day1415;
+import java.util.Arrays;
+import java.util.Comparator;
+public class ArraySort {
+
+    public static void main(String[] args) {
+        String[] strings = {"rohith", "banana", "vivek", "date", "one"};
+
+        Arrays.stream(strings)
+                .sorted(Comparator.comparingInt(String::length)
+                        .thenComparing(Comparator.comparingInt((String s) -> s.charAt(s.length() - 1)).reversed()))
+                .forEach(System.out::println);
+
+    }
+}
+
