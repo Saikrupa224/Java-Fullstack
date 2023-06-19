3c2f8155626879279e2c0f330c3d0b9017a47d9a
saikrupa224 <saikrupareddymiriyala@gmail.com>
2023-06-19 14:51:59 -0500

    Added

---
+++ src/Day1415/StringVowels.java
@@ -0,0 +1,25 @@
+package Day1415;
+import java.util.Arrays;
+import java.util.stream.Stream;
+public class StringVowels {
+
+
+    public static void main(String[] args) {
+        String[] strings = {"apple", "banana", "cherry", "date"};
+
+        Stream<String> vowelPrinting = Arrays.stream(strings).map(s -> {
+            s.toLowerCase();
+            int vowels = 0;
+            for (
+                    int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u')
+                    vowels++;
+
+            }
+            return s + vowels;
+        });
+        vowelPrinting.sorted().forEach(System.out::println);
+
+
+    }
+}
