96403cd455c9c8e8d6065f46749bbbddeab5b6bb
saikrupa224 <saikrupareddymiriyala@gmail.com>
2023-06-22 15:05:47 -0500

    Added

---
+++ src/Day17/distinctchar.java
@@ -0,0 +1,23 @@
+package Day17;
+import java.util.HashMap;
+import java.util.Map;
+public class distinctchar {
+    public static void main(String[] args) {
+        String input = "test string";
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+
+
+        for (char ch : input.toCharArray()) {
+
+            frequencyMap.put(ch, frequencyMap.getOrDefault(ch, 0) + 1);
+        }
+
+        System.out.println("Character Frequencies:");
+        for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+            char ch = entry.getKey();
+            int frequency = entry.getValue();
+            System.out.println(ch + "-" + frequency);
+
+        }
+    }
+}
